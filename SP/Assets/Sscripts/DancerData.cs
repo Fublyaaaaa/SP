d74a8de17621e73095a265bf845f4e1d8bcc579e
KazecDenis <150972276+KazecDenis@users.noreply.github.com>
2025-10-09 06:27:04 +0300

    ну пока так

---
+++ SP/Assets/Sscripts/DancerData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[SerializeField]
+public class DancerData
+{
+public string Name;
+public Vector3 Position;
+public Color Color = Color.white;
+}
---
+++ SP/Assets/Sscripts/StageData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class StageData : MonoBehaviour
+{
+    public int Width;
+    public int Height;
+    public List<DancerData> Dancers;
+}
