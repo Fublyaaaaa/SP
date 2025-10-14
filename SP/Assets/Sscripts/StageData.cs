b6aca3852b7efb490103ab7e3cca808763943863
Fublyaaaaa <superenderbro@mail.ru>
2025-10-14 10:22:14 +0900

    Новая жизнь впереди

--- SP/Assets/Sscripts/StageData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-public class StageData : MonoBehaviour
-{
-    public int Width;
-    public int Height;
-    public List<DancerData> Dancers;
-}
